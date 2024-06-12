0b2a74a6fe82e607c9e13a83530abe3d2ae8112f
fltzr <josh.salinas.j@outlook.com>
2024-06-12 16:18:45 -0400

    chore: Update 404 page layout and styles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,22 +9,24 @@ import { AlignLeftIcon } from "lucide-react";
 
 export const NotFoundPage = () => {
   return (
-    <Card>
-      <CardTitle>404 | Uh oh!</CardTitle>
-      <CardDescription>
-        Sorry love, this is a dead end. If there should be something here,
-        please let me know!
-      </CardDescription>
-      <CardFooter>
-        <Button
-          onClick={() => {
-            window.history.back();
-          }}
-        >
-          <AlignLeftIcon className="mr-2 h-4 w-4" />
-          Go back
-        </Button>
-      </CardFooter>
-    </Card>
+    <div className="w-full flex justify-center items-center">
+      <Card className="w-[400px] h-min-[250px]">
+        <CardTitle>404 | Uh oh!</CardTitle>
+        <CardDescription>
+          Sorry love, this is a dead end. If there should be something here,
+          please let me know!
+        </CardDescription>
+        <CardFooter>
+          <Button
+            onClick={() => {
+              window.history.back();
+            }}
+          >
+            <AlignLeftIcon className="mr-2 h-4 w-4" />
+            Go back
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
